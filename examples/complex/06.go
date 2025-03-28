baa2a5cb769b64e32cf46dfd18543c0ec63742cb
Bartosz Stachowiak <112800510+ZeBartosz@users.noreply.github.com>
2025-03-28 13:42:10 +0000

    update to go

---
+++ examples/complex/06.go
@@ -0,0 +1,20 @@
+package main
+
+// import "fmt"
+
+// func add(x int, y int) int {
+// 	return x + y
+// }
+
+// func main() {
+// 	const message string = "Hello, World!"
+// 	var num1 int = 10
+// 	var num2 int = 5
+
+// 	sum := add(num1, num2)
+// 	product := num1 * num2
+
+// 	fmt.Println(message)
+// 	fmt.Println("Sum:", sum)
+// 	fmt.Println("Product:", product)
+// }
